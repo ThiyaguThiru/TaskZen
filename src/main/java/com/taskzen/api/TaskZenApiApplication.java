ec99da0ac26bdb2178a2cd766916504a977eb5cf
ThiyaguThiru-ADS <thiyaguthiru1000@gmail.com>
2025-09-27 02:22:18 +0530

    Initial commit: Spring Boot TaskZen API setup

---
+++ src/main/java/com/taskzen/api/TaskZenApiApplication.java
@@ -0,0 +1,13 @@
+package com.taskzen.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskZenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskZenApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taskzen/api/TaskZenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskzen.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskZenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
